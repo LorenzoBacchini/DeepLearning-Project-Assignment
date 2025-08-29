74b978cf5c95471279319ac812f62c829c3a7df8
Lorenzo Bacchini <lorenzo.bacchini4@studio.unibo.it>
2025-08-29 12:04:38 +0200

    added to utils_datasets the class able to generate the test dataset with the images concatenated on the channel axis

--- utils_datasets.py
+++ utils_datasets.py
@@ -46,6 +46,46 @@ class DynamicWMNIST(torchvision.datasets.MNIST):
             concat_img = self.transform(concat_img)
 
         return concat_img, number
+    
+class DynamicDMNIST(torchvision.datasets.MNIST):
+    def __init__(self, root="./data", train=True, min_digits=1, max_digits=3, dataset_size=150000, transform=None, download=True):
+        super().__init__(root=root, train=train, transform=transform, download=download)
+
+        self.min_digits = min_digits
+        self.max_digits = max_digits
+        self.dataset_size = dataset_size
+        self.transform = transform
+
+        self.label_to_indices = {
+            i: torch.where(self.targets == i)[0] for i in range(10)
+        }
+
+    def __len__(self):
+        return self.dataset_size
+
+    def __getitem__(self, idx):
+        n_digits = random.randint(self.min_digits,self.max_digits)
+        number = random.randint(0 if n_digits == 1 else 10**(n_digits-1), 10**n_digits - 1)
+        digits = list(str(number))
+
+        digit_images = []
+        for d in digits:
+            label = int(d)
+            indices = self.label_to_indices[label]
+            chosen_idx = indices[torch.randint(len(indices), (1,)).item()]
+            img = self.data[chosen_idx]
+            img = img.unsqueeze(0)
+            digit_images.append(img)
+        for i in range(n_digits, self.max_digits):
+            img = torch.zeros((28, 28))
+            img = img.unsqueeze(0)
+            digit_images.append(img)
+        concat_img = torch.cat(digit_images, dim=0)
+
+        if self.transform:
+            concat_img = self.transform(concat_img)
+
+        return concat_img, number
 
 class testMNISTDataset(Dataset):
     def __init__(self, root, transform=None):
